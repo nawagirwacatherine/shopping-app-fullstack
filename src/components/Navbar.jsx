894b2c0547b1a6f15f8f27e76099eef28c775632
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-09-30 06:46:07 +0200

    Add icons in the search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
+import { CiSearch } from "react-icons/ci";
+import { RiAdminLine } from "react-icons/ri";
+import { GiShoppingCart } from "react-icons/gi";
+import { TbMenuDeep } from "react-icons/tb";
+import { IoClose } from "react-icons/io5";
 import { Link, NavLink } from 'react-router-dom'
 
 
 const Navbar = () => {
-  return (
+
+    const [visible,setVisible] = useState(false);
+      return (
+        <>
     <div className='flex items-center justify-between py-5 font-medium'>
         <img src={assets.product3} className='w-36' alt="" />
 
@@ -33,13 +41,12 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-6'>
-            search image
-            <img src="" className='w-5 cursor-pointer' alt="" />
+           
+   <CiSearch className='w-5 cursor-pointer' />
 
             <div className='group relative'>
                 
-                admin image
-                <img className='w-5 cursor-pointer' src="" alt="" />
+                <RiAdminLine className='w-5 cursor-pointer' />
 
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
@@ -52,14 +59,28 @@ const Navbar = () => {
             </div>
 
             <Link to='/cart' className='relative'>
-            cart image
-            <img src="" className='w-5 min-w-5' alt="" />
+        
+            <GiShoppingCart className='w-5 min-w-5' />
 
             <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
             </Link>
-        </div>
+
+            <TbMenuDeep onClick={()=>setVisible(true)} className='w-5 cursor-pointer sm:hidden'   />
+
+                </div>
         
     </div>
+    {/* Sidebar menu for small screens */}
+
+    <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+        <div className='flex flex-col text-gray-600'>
+            <div  onclick={()=>setVisible(false)} className='flex items-center gap-4 p-3'>
+                <IoClose className='h-4 rotate-180' />
+                <p>Back</p>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 

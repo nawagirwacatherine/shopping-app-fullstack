004935ff0659b9c66f278cf167cf9eab09e27f58
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-10-01 08:45:53 +0200

    Add content in the hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,10 +74,15 @@ const Navbar = () => {
 
     <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
         <div className='flex flex-col text-gray-600'>
-            <div  onclick={()=>setVisible(false)} className='flex items-center gap-4 p-3'>
+            <div  onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
                 <IoClose className='h-4 rotate-180' />
                 <p>Back</p>
             </div>
+
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+            <NavLink  onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
         </div>
     </div>
     </>

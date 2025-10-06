ceb8a190f0b88abd32f9afed1ca42dade372a99d
nawagirwacatherine <cathynawagirwa@gmail.com>
2025-10-06 06:14:20 +0200

    Added more products to assets.jsx file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,27 @@ const Footer = () => {
             <img src={assets.product3} className='mb-5 w-32' alt="" />
             <p className='w-full md:w-2/3 text-gray-600'>Your one-stop shop for quality products delivered to your door. We’re committed to offering great value and fast service.</p>
         </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
         </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>+256-751-172-007</li>
+            <li>contact@katiescollection.com</li>
+          </ul>
+        </div>
+        </div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025@ Katiescollection.com - All Rights Reserved.</p>
     </div>
   )
 }

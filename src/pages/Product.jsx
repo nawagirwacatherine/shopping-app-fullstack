d2498ed69b7f995536b814d1052581c0eda21ed6
nawagirwacatherine <cathynawagirwa@gmail.com>
2025-10-17 06:48:05 +0200

    Adddescription to the items

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -9,7 +9,8 @@ const Product = () => {
   const {productId} = useParams();
   const {products,currency} = useContext(ShopContext);
   const [productData,setProductData] = useState(false);
-  const [image, setImage] = useState('')
+  const [image, setImage] = useState('');
+  const [size, setSize] = useState('');
 
   const fetchProductData = async () => {
 
@@ -70,8 +71,39 @@ const Product = () => {
           <p className='mt-5 text-3xl font-medium'>{currency} {productData.price}</p>
           <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
 
+          <div className='flex flex-col gap-4 my-8'>
+            <p>Select Size</p>
+
+            <div className='flex gap-2'>
+              {productData.sizes.map((item,index) =>(
+                <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+              ))}
+            </div>
+          </div>
+
+          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 '> ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5'/>
+
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+             <p>100% Original product.</p>
+              <p>Cash on delivery is available on this product.</p>
+               <p>Easy return and exchange policy within 7 days.</p>
+          </div>
         </div>
 
+      </div>
+
+      {/* ------Description & Review Section --------- */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm '>Reviews(122)</p>
+        </div>
+
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p> An ecommerce website is an online platform that facilitates the buying and selling.Mutual marketplace wherer business and individuals can showcase their products, interact with the customer. </p>
+          <p>E-commerce websites typically display products or services along with detailed information(sizes, colors). Each product usually has its own dedicated page with relevant information.</p>
+        </div>
       </div>
     </div>
   ): <div className='opacity-0'></div>

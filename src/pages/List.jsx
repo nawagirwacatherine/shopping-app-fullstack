58063e4b77e92e5d2fb40244e64c151d51cf75fb
nawagirwacatherine <cathynawagirwa@gmail.com>
2025-11-03 12:02:38 +0200

    connected backend with home page

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from 'react'
 import { backendUrl, currency } from '../App'
 import axios from 'axios'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const List = ({token}) => {
@@ -35,9 +37,13 @@ const List = ({token}) => {
           toast.success(response.data.message)
           await fetchList();
 
+        } else {
+          toast.error(response.data.message)
         }
               } catch (error) {
         
+                console.log(error)
+                toast.error(error.message)
       }
 
     }
@@ -51,7 +57,7 @@ const List = ({token}) => {
     <>
     <p className='mb-2'>All Products List</p>
 
-    <div className='flex flex-col gap2'>
+    <div className='flex flex-col gap-2'>
       {/* --------List Table Title------- */}
       <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
         <b>Image</b>
@@ -64,12 +70,12 @@ const List = ({token}) => {
       {/* -------Product List-------- */}
       {
         list.map((item,index)=>(
-          <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols[1fr_3fr_1fr_1fr_fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
+          <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
             <img className='w-12' src={item.image[0]} alt="" />
             <p>{item.name}</p>
             <p>{item.category}</p>
             <p>{currency}{item.price}</p>
-            <p className='text-right md:text-center cursor-pointer text-lg'>X</p>
+            <p onClick={()=>removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
 
           </div>
 
